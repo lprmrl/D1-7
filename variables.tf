890331d6586e45476bb98017ef655c9ec1e13bd2
PRMR <you@example.com>
2022-08-24 19:14:00 +0300

    add project D1-7

---
+++ variables.tf
@@ -0,0 +1,17 @@
+#Вынесенные переменные 
+variable "token" {
+	description      = "Dedault folder ID in yandex cloud"
+	type             = string
+	default          = "xxxxxxxxxxxxx"   
+	}
+variable "folder_id" {
+	description      = "Dedault folder ID in yandex cloud"
+	type             = string
+	default          = "xxxxxxxxxxxxxx"   
+	}
+
+variable "cloud_id" {
+	description      = "Dedault cloud ID in yandex cloud"
+	type             = string
+	default          = "xxxxxxxxxxx"   
+}
